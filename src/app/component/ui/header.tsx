6cfa144f60b1b796f9b6a23926b12001f3b6157a
iamjaytru <chukuweikejude7@gmail.com>
2025-08-21 21:43:29 +0100

    fixed logo image

--- src/app/component/ui/header.tsx
+++ src/app/component/ui/header.tsx
@@ -6,21 +6,21 @@ import { MobileMenu } from "./mobile-menu";
 export default function Header() {
   return (
     <header
-      className="fixed top-0 left-0 w-full z-20 bg-[#0a192f] py-4"
+      className="fixed top-0 left-0 w-full bg-[#0a192f] py-0 z-50"
       aria-label="Main navigation"
     >
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center">
           <Link
+            className="text-white font-bold text-2xl sm:text-3xl md:text-5xl hover:text-[#64ffda] transition-colors duration-200"
             href="/"
-            className="text-white font-bold text-2xl sm:text-3xl md:text-4xl hover:text-[#64ffda] transition-colors duration-200"
             aria-label="Homepage"
           >
             <Image
-              src="/images/CJ_logo.png"
+              src="/images/CJ-real.svg"
               alt="Logo"
-              width={40}
-              height={40}
+              width={80}
+              height={80}
             />
           </Link>
 
